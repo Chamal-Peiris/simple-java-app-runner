2f7ba0c16b93e438db49f60b50f4ce99ddeef119
Chamal Peiris <chamal.peiris3g@gmail.com>
2022-02-25 10:33:23 +0530

    Finalize all Functions

---
+++ README.md
@@ -0,0 +1,54 @@
+#Simple Java Application Runner
+
+
+
+
+**This is a Javafx application which uses the java process class to carry out various process's. To be more specific, this application will use the process clas inorder to execute java code then compile, run and provide the necessary outputs. in addition the system will handle both compile time and runtime errors with the aid of getErrorStream() method**
+
+
+### How to use this repo?
+
+1. Clone the repository first  'git clone'
+
+``https://github.com/Chamal-Peiris/simple-java-app-runner.git``
+
+2. Once cloned, open the repository from any prefered IDE
+
+3. Build and Run!
+
+
+### Prerequisites
+1.Use JDK 1.8
+
+## Main User  Interface
+
+![](src/image/MainUI.png)
+
+
+## Compile Time Errors
+
+![](src/image/compilerror.png)
+
+
+## Run Time Errors
+
+![](src/image/runtimeerror.png)
+
+
+## Usage
+
+* This Application Can Be Used to simply compile and run any java code.
+* It will alert you with both compile and runtime errors.
+
+
+### License
+
+Version 1.0 , 25 February 2022
+
+Copyright © copy; 2022 Chamal Peiris. All rights reserved.
+
+Licensed under the [MIT](LICENSE) license
+
+## Acknowledgements
+
+* [Choose an Open Source License](https://choosealicense.com)
--- src/controller/MainFormController.java
+++ src/controller/MainFormController.java
@@ -1,4 +1,66 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class MainFormController {
+    public TextArea txtInputCode;
+    public TextArea txtOutputCode;
+
+    public void btnExecute_OnAction(ActionEvent actionEvent) throws IOException {
+
+        try {
+            /* Step 1 */
+            String data = "public class DEP8IDEDemo{\n" +
+                    "public static void main(String args[]){\n"+
+                    txtInputCode.getText() +
+                    "\n}\n" +
+                    "}";
+
+            /* Step 2 */
+            String tempDir = System.getProperty("java.io.tmpdir");
+            Path tempFilePath = Paths.get(tempDir ,  "DEP8IDEDemo.java");
+            Files.write(tempFilePath, data.getBytes());
+
+            /* Step 3 */
+            Process javac = Runtime.getRuntime().exec("javac " + tempFilePath);
+            int exitCode = javac.waitFor();
+
+            if (exitCode == 0){
+                /* Step 4 */
+                Process java = Runtime.getRuntime().exec("java -cp " + tempDir + " DEP8IDEDemo");
+                exitCode = java.waitFor();
+
+                if (exitCode == 0){
+                    readStream(java.getInputStream());
+                }else{
+                    readStream(java.getErrorStream());
+                }
+            }else{
+                readStream(javac.getErrorStream());
+            }
+
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            Path classFilePath = Paths.get(System.getProperty("java.io.tmpdir"), "DEP8IDEDemo.class");
+            Path javaFilePath = Paths.get(System.getProperty("java.io.tmpdir"), "DEP8IDEDemo.java");
+            Files.deleteIfExists(classFilePath);
+            Files.deleteIfExists(javaFilePath);
+        }
+    }
+
+    private void readStream(InputStream is) throws IOException {
+        byte[] buffer = new byte[is.available()];
+        is.read(buffer);
+        txtOutputCode.setText(new String(buffer));
+        is.close();
+    }
+
 }
